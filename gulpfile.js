098528da308472abc566acb608c95a507c7c06e8
Marcelo Paiva <mpaiva@gmail.com>
2021-06-04 07:21:50 -0400

    gulp 4 update
    gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,39 @@
-var gulp             = require('gulp'),
-    sass             = require('gulp-sass'),
-    autoprefixer     = require('gulp-autoprefixer'),
-    sourcemaps       = require('gulp-sourcemaps'),
-    minifyCss        = require('gulp-clean-css'),
-    rename           = require('gulp-rename'),
-    browserSync      = require('browser-sync').create();
-    livereload       = require('gulp-livereload');
-
-
-
-gulp.task('sass', function(){
-    // sass directory
-    return gulp.src('./scss/*scss')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var minifyCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var livereload = require('gulp-livereload');
+	
+function style() {
+    return (
+        gulp
+            .src("./scss/*.scss")
+ 
+            // Use sass with the files found, and log any errors
             .pipe(sass())
-            //outputstyle (nested, compact, expanded, compressed)
-            .pipe(sass({outputStyle:'compact'}).on('error', sass.logError))
-            // sourcemaps
-            .pipe(sourcemaps.init())
-            // sourcemaps output directory
-            .pipe(sourcemaps.write(('./maps')))
-            // css output directory
-            .pipe(gulp.dest('./src/css'))
-            .pipe(livereload()),
-            // watch file
-            gulp.watch('./scss/*.scss', ['sass']);
-});
-
-
-// minify css (merge + autoprefix + rename)
-gulp.task('minify-css', function(){
-   return gulp.src('./src/css/style.css')
-            .pipe(minifyCss())
-             // autoprefixer
-            .pipe(autoprefixer({
-                browsers: ['last 15 versions'],
-                cascade: false
-            }))
-            // minify output directory
-            .pipe(rename('style.min.css'))
-            .pipe(gulp.dest('./src/css'))
-            // browser sync
-            .pipe(browserSync.reload({stream: true}))
-            .pipe(livereload()),
-            // watch file
-            livereload.listen();
-            gulp.watch('./src/css/style.css', ['minify-css']);
-});
-
-
-// sass/css browser tracking
-gulp.task('browser-sync', function(){
+            .on("error", sass.logError)
+ 
+            // What is the destination for the compiled file?
+            .pipe(gulp.dest("./src/css"))
+            .pipe(browserSync.stream())
+    );
+}
+	
+function reload() {
+    browserSync.reload();
+}
+ 
+function watch(){	
     browserSync.init({
-        server:{
-            baseDir: './src'
+        server: {
+            baseDir: "./src/"
         }
     });
-    // watch html
-    livereload.listen(reloadPage);
-    gulp.watch('./src/*.html').on('change', browserSync.reload);
-});
+    gulp.watch('./scss/*.scss', style);
+    gulp.watch("./src/*.html", reload);
+}
 
-// gulp default (sass, minify-css, browser-sync) method
-gulp.task('default', ['sass', 'minify-css', 'browser-sync']);
+exports.watch = watch;
